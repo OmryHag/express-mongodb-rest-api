ad1101975baec46c6da9f9b8d36698c1bad82de9
OmryH <omry.hagag@gmail.com>
2023-05-01 16:53:39 -0600

    inital error handling

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv').config();
+const {errorHandler} = require('./middleware/errorMiddleware');
 const port = process.env.PORT || 5000;
 
 const app = express();
+
+// handle body
+app.use(express.json());
+app.use(express.urlencoded({extended: false}));
+
+// handle error
+app.use(errorHandler);
 
 app.use('/api/goals', require('./routes/goalRoutes'));
 
