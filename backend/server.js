162c66802e71dc93212bcc003cd059f23c836a56
OmryH <omry.hagag@gmail.com>
2023-05-03 16:09:50 -0600

    Added tokens to user

--- backend/server.js
+++ backend/server.js
@@ -13,10 +13,11 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
 
-// handle error
-app.use(errorHandler);
 
 app.use('/api/goals', require('./routes/goalRoutes'));
 app.use('/api/users', require('./routes/userRoutes'));
 
 app.listen(port, () => console.log(`Server started in port ${port}`));
+
+// handle error
+app.use(errorHandler);
